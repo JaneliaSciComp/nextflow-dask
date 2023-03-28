617df6e78f4302573463d0379e58d19bcccbd5e6
Cristian Goina <goinac@hhmi.org>
2023-03-28 11:40:38 -0400

    newer dask based containers - 2023.3.2

--- containers/create_container.sh
+++ containers/create_container.sh
@@ -1,4 +1,4 @@
 docker build \
-    -t registry.int.janelia.org/janeliascicomp/dask:2022.12.1 \
-    -t janeliascicomp/dask:2022.12.1 \
+    -t registry.int.janelia.org/janeliascicomp/dask:2023.3.2 \
+    -t janeliascicomp/dask:2023.3.2 \
     $*
